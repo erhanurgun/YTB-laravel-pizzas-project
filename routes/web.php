700ca4a391f6512942d20b8eb919365d29c810f2
PCI-ACER\erhan <erhan.rgn04@gmail.com>
2022-08-24 03:31:27 +0300

    lesson #3: Laravel Basics (Routes, Views)

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pizzas', function () {
+    return view('pizzas');
+    // return 'pizzas!';
+    /*return [
+        'name' => 'veg pizzas',
+        'base' => 'classic'
+    ];*/
+});
