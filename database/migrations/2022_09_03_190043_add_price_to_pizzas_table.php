9ab31e0cabbf8671bbe5d79551bb1880d618d538
PCI-ACER\erhan <erhan.rgn04@gmail.com>
2022-09-03 22:11:20 +0300

    lesson: #14 - More on Migrations

--- database/migrations/2022_09_03_190043_add_price_to_pizzas_table.php
+++ database/migrations/2022_09_03_190043_add_price_to_pizzas_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -12,13 +13,8 @@ return new class extends Migration {
      */
     public function up()
     {
-        Schema::create('pizzas', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->string('type');
-            $table->string('base');
-            $table->string('name');
-            // $table->integer('price');
+        Schema::table('pizzas', function (Blueprint $table) {
+            $table->integer('price');
         });
     }
 
@@ -29,6 +25,8 @@ return new class extends Migration {
      */
     public function down()
     {
-        Schema::dropIfExists('pizzas');
+        Schema::table('pizzas', function (Blueprint $table) {
+            $table->integer('price');
+        });
     }
 };
