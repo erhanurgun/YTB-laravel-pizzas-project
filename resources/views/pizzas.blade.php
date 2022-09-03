30f5ba213a3ccff96258cdc410165df6c5fb621e
PCI-ACER\erhan <erhan.rgn04@gmail.com>
2022-09-03 17:59:41 +0300

    lesson #8: CSS & Images

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -4,6 +4,7 @@
 <div
     class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+
         <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
             <h1 class="font-size-50px"><span>Pizza</span>'s List</h1>
         </div>
